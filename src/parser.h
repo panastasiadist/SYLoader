7444e222c171dc3972f79fadd471bd0745c3b872
Panagiotis Anastasiadis <panastas91@gmail.com>
2015-07-30 19:57:56 +0300

    Fix: Copyright notice

--- src/parser.h
+++ src/parser.h
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Copyright 2015 Panagiotis Anastasiadis
- * This file is part of eTube Downloader.
+ * This file is part of SYLoader.
  *
  * SYLoader is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
