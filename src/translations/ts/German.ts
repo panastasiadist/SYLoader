9b322e45a14263768ed48806802c28af8b1370dc
Philip <zeruxky.contact@yahoo.de>
2017-06-25 13:53:55 +0200

    German translation
    Proved it with the latest version of SYLoader

---
+++ src/translations/ts/German.ts
@@ -0,0 +1,474 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_AT">
+<context>
+    <name>AboutForm</name>
+    <message>
+        <location filename="../about_form.ui" line="83"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../about_form.ui" line="98"/>
+        <source>SYLoader</source>
+        <translation>SYLoader</translation>
+    </message>
+    <message>
+        <location filename="../about_form.ui" line="105"/>
+        <source>Brought to you by Panagiotis Anastasiadis. SYLoader is dedicated to a special girl. Enjoy!</source>
+        <translation>Bereitgestellt von Panagiotis Anastasiadis. SYLoader ist einem speziellem Mädchen gewidmet. Viel Spaß!</translation>
+    </message>
+    <message>
+        <location filename="../about_form.ui" line="115"/>
+        <source>Contributors</source>
+        <translation>Beitragende</translation>
+    </message>
+    <message>
+        <location filename="../about_form.ui" line="125"/>
+        <source>Visit Project&apos;s Page</source>
+        <translation>Projektseite besuchen</translation>
+    </message>
+    <message>
+        <location filename="../about_form.ui" line="132"/>
+        <source>Contact Developer</source>
+        <translation>Entwickler kontaktieren</translation>
+    </message>
+    <message>
+        <location filename="../about_form.ui" line="139"/>
+        <source>View GPL License</source>
+        <translation>GPL Lizenz ansehen</translation>
+    </message>
+    <message>
+        <location filename="../about_form.ui" line="146"/>
+        <source>Licensed under the terms and conditions of the GNU GPL v2</source>
+        <translation>Lizenziert unter den Bestimmungen und Konditionen der GNU GPL v2</translation>
+    </message>
+    <message>
+        <location filename="../about_form.ui" line="156"/>
+        <source>This product includes software developed by the OpenSSL Project for use in the OpenSSL Toolkit (http://www.openssl.org/)</source>
+        <translation>Dieses Projekt enthält Software, die vom OpenSSL Projekt für das OpenSSL Toolkit entwickelt wurde (http://www.openssl.org/)</translation>
+    </message>
+</context>
+<context>
+    <name>MainForm</name>
+    <message>
+        <location filename="../main_form.ui" line="78"/>
+        <source>SYLoader</source>
+        <translation>SYLoader</translation>
+    </message>
+    <message>
+        <location filename="../main_form.ui" line="157"/>
+        <source>Show downloads directory</source>
+        <translation>Zeige Downloadverzeichnis</translation>
+    </message>
+    <message>
+        <location filename="../main_form.ui" line="186"/>
+        <source>Download selected video</source>
+        <translation>Lade ausgewähltes Video</translation>
+    </message>
+    <message>
+        <location filename="../main_form.ui" line="215"/>
+        <source>Cancel downloading selected video</source>
+        <translation>Unterbreche das Herunterladen des ausgewählten Videos</translation>
+    </message>
+    <message>
+        <location filename="../main_form.ui" line="241"/>
+        <source>Delete only the selected video from the list</source>
+        <translation>Nur das ausgewählte Video von der Liste löschen</translation>
+    </message>
+    <message>
+        <location filename="../main_form.ui" line="270"/>
+        <source>Empty the list</source>
+        <translation>Liste löschen</translation>
+    </message>
+    <message>
+        <location filename="../main_form.ui" line="313"/>
+        <source>Choose the file format for your downloaded videos</source>
+        <translation>Wähle das Dateiformat für den Videodownload</translation>
+    </message>
+    <message>
+        <location filename="../main_form.ui" line="333"/>
+        <location filename="../main_form.cpp" line="518"/>
+        <source>Download</source>
+        <translation>Herunterladen</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="131"/>
+        <source>Title</source>
+        <translation>Title</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="132"/>
+        <source>Status</source>
+        <translation>Status</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="133"/>
+        <source>Progress</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="134"/>
+        <source>Speed</source>
+        <translation>Fortschritt</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="135"/>
+        <source>ETA</source>
+        <translation>Voraussichtlich</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="161"/>
+        <source>MP3 Music</source>
+        <translation>MP3 Musik</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="167"/>
+        <source>MP4 Video</source>
+        <translation>MP4 Video</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="173"/>
+        <source>AVI Video</source>
+        <translation>AVI Video</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="179"/>
+        <source>OGG Music</source>
+        <translation>OGG Musik</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="185"/>
+        <source>OGV Video</source>
+        <translation>OGG Video</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="272"/>
+        <location filename="../main_form.cpp" line="287"/>
+        <location filename="../main_form.cpp" line="318"/>
+        <location filename="../main_form.cpp" line="368"/>
+        <location filename="../main_form.cpp" line="387"/>
+        <location filename="../main_form.cpp" line="566"/>
+        <source>Information</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="273"/>
+        <source>Hooray! This download is already complete!</source>
+        <translation>Hurra! Der Download ist schon abgeschlossen!</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="288"/>
+        <source>This download is already in progress.</source>
+        <translation>Dieses Video ist schon in Bearbeitung.</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="319"/>
+        <source>This download is not currently in progress.</source>
+        <translation>Dieser Download ist derzeit nicht in Bearbeitung.</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="369"/>
+        <source>This download is in progress. Please cancel it first.</source>
+        <translation>Dieser Download ist in Bearbeitung. Bitte breche diesen zuerst ab.</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="388"/>
+        <source>One or more downloads are in progress. Please cancel them first.</source>
+        <translation>Eine oder mehrere Downloads sind in Bearbeitung. Bitte breche diese zuerst ab.</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="446"/>
+        <source>Queued</source>
+        <translation>Eingereiht</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="449"/>
+        <source>Downloading</source>
+        <translation>Ladet herunter</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="452"/>
+        <source>Converting</source>
+        <translation>Konvertiert</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="455"/>
+        <source>Complete</source>
+        <translation>Abgeschlossen</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="458"/>
+        <source>I/O Error</source>
+        <translation>I/O Fehler</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="461"/>
+        <source>Connection Error</source>
+        <translation>Verbindungsfehler</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="464"/>
+        <source>Canceled</source>
+        <translation>Abgebrochen</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="504"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="527"/>
+        <source>One or more downloads have failed due to Internet connection problems. Check your Internet connection and try again!</source>
+        <translation>Eine oder mehrere Downloads sind aufgrund von Verbindungsfehler mit dem Internet fehlgeschlagen. Überprüfe deine Internetverbindung und versuche es nochmal!</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="530"/>
+        <location filename="../main_form.cpp" line="536"/>
+        <source>Warning</source>
+        <translation>Warnung</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="534"/>
+        <source>One or more downloads have failed because their conversion failed.</source>
+        <translation>Eine oder mehrere downloads sind fehlgeschlagen, da ihre Umwandlung fehlgeschlagen ist.</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="547"/>
+        <source>Congratulations!</source>
+        <translation>Glückwunsch!</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="548"/>
+        <source>You have completed your first download. Would you like to support SYLoader by telling your friends?</source>
+        <translation>Du hast deinen ersten Download abgeschlossen. Willst du SYLoader unterstützen, indem du es deinen Freunden weiterempfiehlst?</translation>
+    </message>
+    <message>
+        <location filename="../main_form.cpp" line="565"/>
+        <source>Hooray! Your downloads have been completed.</source>
+        <translation>Hurra! Deine Downloads sind abgeschlossen.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../main_window.ui" line="440"/>
+        <source>SYLoader</source>
+        <translation>SYLoader</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="510"/>
+        <source>Show the download list</source>
+        <translation>Zeige Downloadliste</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="539"/>
+        <source>Show settings</source>
+        <translation>Zeige Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="568"/>
+        <source>Show information about SYLoader</source>
+        <translation>Zeige Informationen über SYLoader</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="597"/>
+        <source>SYLoader on Facebook!</source>
+        <translation>SYLoader auf Facebook!</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="626"/>
+        <source>SYLoader on Twitter!</source>
+        <translation>SYLoader auf Twitter!</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="703"/>
+        <location filename="../main_window.cpp" line="202"/>
+        <location filename="../main_window.cpp" line="310"/>
+        <source>Downloads</source>
+        <translation>Downloads</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="128"/>
+        <location filename="../main_window.cpp" line="183"/>
+        <source>Download by copying YouTube URLs in your browser!</source>
+        <translation>Kopiere YouTube URLs in deinen Browser, um herunterzuladen</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="143"/>
+        <source>You have one or more downloads in progress. You should cancel them first.</source>
+        <translation>Du hast einen oder mehrere Downloads in Bearbeitung. Breche diese zuerst ab.</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="145"/>
+        <source>Information</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="178"/>
+        <source>Preparing your new downloads...</source>
+        <translation>Bereite deinen neuen Download vor...</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="212"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="222"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="258"/>
+        <source>New version available!</source>
+        <translation>Neue Version erhältlich!</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="259"/>
+        <source>SYLoader has a new version available. You are advised to get the new improved version. Go to download page?</source>
+        <translation>SYLoader hat eine neue Version verfügbar. Es wird empfohlen, die neue, verbesserte Version zu holen. Zur Downloadseite gehen?</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="291"/>
+        <source>Required Updates</source>
+        <translation>Benötigtes Update</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsForm</name>
+    <message>
+        <location filename="../settings_form.ui" line="83"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../settings_form.ui" line="114"/>
+        <source>Automatic Update Check</source>
+        <translation>Automatische Update Überprüfung</translation>
+    </message>
+    <message>
+        <location filename="../settings_form.ui" line="121"/>
+        <location filename="../settings_form.ui" line="159"/>
+        <source>Enabled</source>
+        <translation>Aktiviert</translation>
+    </message>
+    <message>
+        <location filename="../settings_form.ui" line="128"/>
+        <source>Language</source>
+        <translation>Sprache</translation>
+    </message>
+    <message>
+        <location filename="../settings_form.ui" line="138"/>
+        <source>Concurrent Downloads</source>
+        <translation>Derzeitiger Download</translation>
+    </message>
+    <message>
+        <location filename="../settings_form.ui" line="152"/>
+        <source>Autostart Downloads</source>
+        <translation>Starte Downloads automatisch</translation>
+    </message>
+    <message>
+        <location filename="../settings_form.ui" line="166"/>
+        <source>Downloads Directory</source>
+        <translation>Downloadverzeichnis</translation>
+    </message>
+    <message>
+        <location filename="../settings_form.ui" line="182"/>
+        <source>Browse</source>
+        <translation>Durchsuchen</translation>
+    </message>
+    <message>
+        <location filename="../settings_form.ui" line="210"/>
+        <source>Save Settings</source>
+        <translation>Speichere Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../settings_form.cpp" line="107"/>
+        <source>Select downloads directory</source>
+        <translation>Wähle Downloadverzeichnis</translation>
+    </message>
+    <message>
+        <location filename="../settings_form.cpp" line="138"/>
+        <source>Information</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <location filename="../settings_form.cpp" line="139"/>
+        <source>Your settings have been updated!</source>
+        <translation>Ihre Einstellungen wurden übernommen!</translation>
+    </message>
+</context>
+<context>
+    <name>UpdateForm</name>
+    <message>
+        <location filename="../update_form.ui" line="434"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../update_form.ui" line="455"/>
+        <source>Installing required updates. Please wait...</source>
+        <translation>Installiere benötigtes Update. Bitte warten...</translation>
+    </message>
+</context>
+<context>
+    <name>Processor</name>
+    <message>
+        <location filename="../processor.cpp" line="82"/>
+        <source>A/S Error</source>
+        <translation>A/S Fehler</translation>
+    </message>
+    <message>
+        <location filename="../processor.cpp" line="213"/>
+        <source>Already Downloaded</source>
+        <translation>Bereit heruntergeladen</translation>
+    </message>
+    <message>
+        <location filename="../processor.cpp" line="701"/>
+        <source>Queued</source>
+        <translation>Eingereiht</translation>
+    </message>
+    <message>
+        <location filename="processor.cpp" line="418"/>
+        <source>Ready</source>
+        <translation>Bereit</translation>
+    </message>
+    <message>
+        <location filename="processor.cpp" line="421"/>
+        <source>Downloading</source>
+        <translation>Herunterladen</translation>
+    </message>
+    <message>
+        <location filename="processor.cpp" line="424"/>
+        <source>Converting</source>
+        <translation>Konvertieren</translation>
+    </message>
+    <message>
+        <location filename="processor.cpp" line="427"/>
+        <source>Complete</source>
+        <translation>Angeschlossen</translation>
+    </message>
+    <message>
+        <location filename="processor.cpp" line="430"/>
+        <source>I/O Error</source>
+        <translation>I/O Fehler</translation>
+    </message>
+    <message>
+        <location filename="processor.cpp" line="433"/>
+        <source>Connection Error</source>
+        <translation>Verbindungsfehler</translation>
+    </message>
+    <message>
+        <location filename="processor.cpp" line="436"/>
+        <source>Canceled</source>
+        <translation>Abgebrochen</translation>
+    </message>
+    <message>
+        <location filename="processor.cpp" line="456"/>
+        <location filename="processor.cpp" line="463"/>
+        <source>N/A</source>
+        <translation>Nicht verfügbar</translation>
+    </message>
+</context>
+</TS>
